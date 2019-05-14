da7c0a1f54b21c85fc229cd2baa35123755db2de
Mihrimah K <mihrimahk@gmail.com>
2019-05-13 22:53:00 -0400

    initial commit

---
+++ dbase/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ('SIGNATURE BURGER');
+INSERT INTO burgers (burger_name) VALUES ('BLUE MOON BURGER');
+INSERT INTO burgers (burger_name) VALUES ('HEAVY HITTER BURGER');
+INSERT INTO burgers (burger_name) VALUES ('ACTION BURGER');
